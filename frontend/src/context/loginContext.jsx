2067f4f59d6839d5eda384b563926d78f4ba7ada
Saloni <saloni1404999@gmail.com>
2026-04-21 12:15:02 +0530

    Updated

--- frontend/src/context/loginContext.jsx
+++ frontend/src/context/loginContext.jsx
@@ -1,10 +1,19 @@
-import { createContext , useState } from "react";
+import { createContext , useEffect, useState  } from "react";
 import axios from "axios";
 import server from "../api";
+
+
 export const LoginContext = createContext();
 
 export const LoginProvider = ({children}) => {
+
     const [loginUser, setLoginUser] = useState(null);
+    useEffect(()=>{
+        const user = localStorage.getItem("user");
+        if(user){
+            setLoginUser(JSON.parse(user));
+        }
+    },[])
     const login = async (email , password)=>{
         try{
         const res = await axios.post(server + "/login",{
@@ -12,12 +21,18 @@ export const LoginProvider = ({children}) => {
             password
         });
         setLoginUser(res.data);
+        localStorage.setItem("user", JSON.stringify(res.data));
         return res.data;
     }catch(e){
         return e.response.data;
     }
+}
+    const logout = ()=>{
+          localStorage.removeItem("user");
+         setLoginUser(null);
     }
-    return<LoginContext.Provider value={{loginUser , login}}>
+
+    return<LoginContext.Provider value={{loginUser , login , logout}}>
         {children}
     </LoginContext.Provider>
 }
